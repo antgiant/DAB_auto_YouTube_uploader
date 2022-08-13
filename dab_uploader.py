3e6e388d9f806620b1432386de69f8879045b05a
antgiant <antgiant@gmail.com>
2022-08-13 16:54:58 -0400

    Small refactor to clean things up and simplify adding error handling code later

--- dab_uploader.py
+++ dab_uploader.py
@@ -3,20 +3,35 @@ from urllib.parse import urlparse
 from os.path import splitext
 import requests
 import os
-NewsFeed = feedparser.parse("http://feeds.feedburner.com/dailyaudiobiblekids.xml")
-i = 0
-for entry in NewsFeed.entries :
-	i += 1
-	print("Title: " + entry["title"])
-	print("Subtitle: " + entry["subtitle"])
-	for link in entry["links"] :
-		if link["rel"] == "enclosure" :
-			parsed = urlparse(link["href"])
-			root, ext = splitext(parsed.path)
-			response = requests.get(link["href"])
-			open(entry["episodeid"] + ext, "wb").write(response.content)
-			if os.path.exists(entry["episodeid"] + ext):
-				print("Do Stuff Here")
-				os.remove(entry["episodeid"] + ext)
-	if (i > 0) :
-		break
+
+def download_episode(url, filename):
+	"""
+	Function to download the audio files from Podcast episodes
+	"""
+	response = requests.get(url)
+	open(filename, "wb").write(response.content)
+
+def podcast_processor(url, limit = -1):
+	"""
+	Function to parse podcast URLs, turn audio episodes into videos, and upload them to YouTube
+	"""
+	NewsFeed = feedparser.parse(url)
+	i = 0
+	for entry in NewsFeed.entries :
+		i += 1
+		print("Title: " + entry["title"])
+		print("Subtitle: " + entry["subtitle"])
+		for link in entry["links"] :
+			if link["rel"] == "enclosure" :
+		
+				parsed = urlparse(link["href"])
+				root, ext = splitext(parsed.path)
+				filename = entry["episodeid"] + ext
+				download_episode(link["href"], filename)
+				if os.path.exists(filename):
+					print("Do Stuff Here")
+					os.remove(filename)
+		if (limit != -1 and i >= limit) :
+			break
+
+podcast_processor("http://feeds.feedburner.com/dailyaudiobiblekids.xml", 1)
